200a76354c95da83d807c3caacb0ed27da7eec72
Sododa <sophieododa@gmail.com>
2024-05-16 17:04:57 +0000

    test

---
+++ python/area_calc.py
@@ -0,0 +1,9 @@
+#name = input('Tell me your name,punk:')
+#age = input('.....and your age:')
+#print(name,'you are', age)
+
+
+# calc area of a circle
+radius = input ('Enter the radius of your circle (m):')
+area = 3.142 * int(radius)** 2
+print('the area of a circleis:',area)
---
+++ python/if_statements.py
@@ -0,0 +1,5 @@
+age = int(input('Enter your age:'))
+if age < 10:
+    # code block
+    print("you are young,strange one")
+
