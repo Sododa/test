cfc6cb0963fceac937225daf68ba31fc55d73403
Sododa <sophieododa@gmail.com>
2024-05-21 09:24:41 +0000

    while looops

--- python/if_statements.py
+++ python/if_statements.py
@@ -1,5 +1,15 @@
-age = int(input('Enter your age:'))
-if age < 10:
+#age = int(input('Enter your age:'))
+#if age < 10:
     # code block
-    print("you are young,strange one")
-
+ #   print("you are young,strange one")
+#elif age < 40:
+ #   print('the fire in you is strong, strange one')
+#else:
+#   print('you are wise before doubt')
+meaty = input('do you it meet?(y/n)')
+if meaty == 'y':
+    print("here is the meaty menu")
+else:
+    print("here is the veggie menu...")
+    # operators
+    # <, >, ==, != => =<
